bcef8eb041db906dce93c870c0440afe16dc3850
164p <gapstyle1@gmail.com>
2024-01-02 14:02:05 +0700

    web-foundation
    implement Next.js/Tailwind/Typescript/Prisma/MongoDB

---
+++ web-app/prisma/index.mjs
@@ -0,0 +1,36 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+// use `prisma` in your application to read and write data in your DB
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      name: 'Rich',
+      email: 'hello@prisma.com',
+      posts: {
+        create: {
+          title: 'My first post',
+          body: 'Lots of really interesting stuff',
+          slug: 'my-first-post',
+        },
+      },
+    },
+  })
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  })
+  console.dir(allUsers, { depth: null })
+}
+
+main()
+  .catch(async (e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
