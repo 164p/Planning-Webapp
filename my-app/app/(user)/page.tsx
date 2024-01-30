040768787fbbaf85e2b30c34cad7e99bda39c5c1
First1zaza <69394299+First1zaza@users.noreply.github.com>
2024-01-30 23:33:16 +0700

    Fix DB Structure

--- my-app/app/(user)/page.tsx
+++ my-app/app/(user)/page.tsx
@@ -54,8 +54,11 @@ export default function page(){
                     </div>
                 </div>
                 <div className="section">
-                    <div className="card bg-[#EFE5D5] border border-[#674F04] rounded-md">
-                        <p className="text-center">Ready for new journey?</p>
+                    <div className="card bg-[#EFE5D5] border-2 border-[#674F04] rounded-md p-3">
+                        <Link href={"/"} className="flex justify-end items-center">
+                            SEE ALL <span><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M13.3 17.275q-.3-.3-.288-.725t.313-.725L16.15 13H5q-.425 0-.713-.288T4 12q0-.425.288-.713T5 11h11.15L13.3 8.15q-.3-.3-.3-.713t.3-.712q.3-.3.713-.3t.712.3L19.3 11.3q.15.15.213.325t.062.375q0 .2-.063.375t-.212.325l-4.6 4.6q-.275.275-.687.275t-.713-.3Z"></path></svg></span>
+                        </Link>
+                        <p className="font-semibold text-center">Ready for new journey?</p>
                     </div> 
                 </div>
             </div>
