4ce8f18338ceb58b99e4784941aa7fc6674e9fa9
Baixi1 <102402843+Baixi1@users.noreply.github.com>
2024-01-31 22:43:38 +0700

    Update
    Navbar Explore Profile Footer

---
+++ my-app/app/(user)/profile/page.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import { FaUserCircle } from "react-icons/fa"
+import { IoSettingsOutline } from "react-icons/io5"
+
+export default function Home() {
+    return(
+        <main className="mt-60 my-40">
+            <div className="w-full lg:w-6/12 px-4 mx-auto">
+            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg mt-16">
+                <div className="px-6">
+                    <div className="absolute top-8 right-8">
+                        <Link href="/profileset"><IoSettingsOutline className="text-black text-2xl" /></Link>
+                    </div>
+                <div className="flex flex-wrap justify-center">
+                    <div className="w-full px-4 flex justify-center">
+                    <div className="relative">
+                        <FaUserCircle className="text-9xl shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px"/>
+                    </div>
+                    </div>
+                    <div className="flex justify-center py-4 lg:pt-4 pt-8 w-full px-4 text-center mt-20">
+                        <p className="text-3xl font-bold">USERNAME</p>
+                    </div>
+                </div>
+                <div className="mt-10 py-10 border-t border-blueGray-200 text-center">
+                    <div className="flex flex-wrap justify-center">
+                    <div className="w-full lg:w-9/12 px-4">
+                        <p className="mb-4 text-lg leading-relaxed text-blueGray-700">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </main>
+    );
+
+}
