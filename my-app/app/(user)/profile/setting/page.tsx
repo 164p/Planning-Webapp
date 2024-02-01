7cbfc844d9e314c8f8b457cf05cd9fab0559a4f4
164p <gapstyle1@gmail.com>
2024-02-01 11:03:15 +0700

    update img,link

--- my-app/app/(user)/profile/setting/page.tsx
+++ my-app/app/(user)/profile/setting/page.tsx
@@ -18,15 +18,23 @@ export default function Home() {
                             </span>
                         </div>
                     </div>
+                    <div className="card-header relative mb-10">
+                        <p className="text-2xl font-bold text-center">USERNAME</p>
+                    </div>
                     <div className="card-body">
-                        <div className="card-section mx-auto max-w-md grid grid-cols-2 gap-3">
+                        <div className="card-section mx-auto max-w-md">
+                            <div className="col w-1/2 mx-auto">
+                                 <button className="rounded-full w-full bg-[#674F04] text-white font-bold py-2 px-5 text-center">CHANGE</button>     {/*TEMPORARY!!!!*/}
+                            </div>
+                        </div>                         
+                        {/* <div className="card-section mx-auto max-w-md grid grid-cols-2 gap-3">
                             <div className="col">
                                 <button className="rounded-full w-full bg-[#674F04] text-white font-bold py-2 px-5 text-center">CHANGE</button>
                             </div>
                             <div className="col">
                                 <button className="rounded-full w-full bg-[#F5F0E8] text-[#674F04] font-bold py-2 px-5 text-center">DELETE</button>
                             </div>
-                        </div>
+                        </div> */}
                         <hr className="h-px border-none bg-[#674F04] my-10" />
                         <div className="card-section">
                             <p className="mb-4 text-center">
