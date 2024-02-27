668971830fd4cd48a8f11d60a9fb5490d29720e2
First1zaza <69394299+First1zaza@users.noreply.github.com>
2024-02-27 22:52:30 +0700

    Update Navbar.tsx

--- my-app/app/components/Navbar.tsx
+++ my-app/app/components/Navbar.tsx
@@ -66,13 +66,13 @@ export default function Navbar() {
                                             </Link>
                                             
                                         </li>
-                                        <li className='dropdown ml-1.5 flex items-center relative'>
+                                        <li className='dropdown ml-2 flex items-center relative'>
                                             <button onClick={() => setDropDown((prevDisplay) => !prevDisplay)} type='button' className='dropdown-btn'>
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M12 14.975q-.2 0-.388-.075t-.312-.2l-4.6-4.6q-.275-.275-.275-.7t.275-.7q.275-.275.7-.275t.7.275l3.9 3.9l3.9-3.9q.275-.275.7-.275t.7.275q.275.275.275.7t-.275.7l-4.6 4.6q-.15.15-.325.213t-.375.062Z"></path></svg>
                                             </button>
                                             <div className={
-                                                (dropDownShow ? "md:block md:right-0 md:-bottom-16 absolute" : "md:hidden")+
-                                                ' dropdown-body absolute p-3 bg-white rounded-md shadow text-slate-600'
+                                                (dropDownShow ? "md:block md:right-0 md:-bottom-14 absolute" : "md:hidden")+
+                                                ' dropdown-body absolute px-3 py-1.5 bg-white rounded-md shadow text-slate-600'
                                                 }>
                                                 asda
                                             </div>
