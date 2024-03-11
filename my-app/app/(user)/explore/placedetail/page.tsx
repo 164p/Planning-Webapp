d7d9253942d9bef2e5257dfec9a39e7b769715d2
164p <gapstyle1@gmail.com>
2024-03-12 00:42:22 +0700

    sprint2 big update

--- my-app/app/(user)/explore/placedetail/page.tsx
+++ my-app/app/(user)/explore/placedetail/page.tsx
@@ -1,8 +1,11 @@
 import PlaceDetail from "@/app/components/PlaceDetail"
 import Comment from "@/app/components/Comment"
+import React, { useState, useEffect } from 'react';
 
 
+const fetcher = (url : string) => fetch(url).then(r => r.json())
 export default function Home() {
+ 
   return (
     <main className="container pb-12 pt-16">
       
