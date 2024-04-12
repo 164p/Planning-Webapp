ee921521e46c6506e0d15f9d87001d16c5621a69
164p <gapstyle1@gmail.com>
2024-04-13 01:30:12 +0700

    update location add planning

--- my-app/app/(user)/journey/page.tsx
+++ my-app/app/(user)/journey/page.tsx
@@ -1,23 +1,31 @@
-'use client'
-import Jmap from '@/app/components/Jmap'
+"use client";
+import Jmap from "@/app/components/Jmap";
+import React from "react";
+import VectorMap, {
+  Layer,
+  Export,
+  Title,
+  Label,
+  ILayerProps,
+} from "devextreme-react/vector-map";
+export default function Journey() {
+  return (
+    <div>
+      <h1 className="text-[#674F04] text-6xl pt-60 p-10 text-center font-medium">
+        Diary's Journey
+      </h1>
+      <div className="flex justify-center items-center">
+        <div className="flex w-full p-0.5 mt-10 mb-20 lg:w-2/3 bg-[#674F04] " />
+      </div>
+      <div className="container mx-auto py-8">
+        <div className="grid lg:grid-cols-2 grid-cols-1">
+          <div className="">
+            <Jmap />
+          </div>
 
-export default function Journey(){
-    return(
-        <div>
-            <h1 className='text-[#674F04] text-6xl pt-60 p-10 text-center font-medium'>Diary's Journey</h1>
-            <div className='flex justify-center items-center'>
-                <div className='flex w-full p-0.5 mt-10 mb-20 lg:w-2/3 bg-[#674F04] '/>
-            </div>
-            <div className='container mx-auto py-8'>
-                <div className='grid lg:grid-cols-2 grid-cols-1'>
-                    <div className=''>
-                        <Jmap/>
-                    </div>
-                    
-                    <div className='bg-[#674F04] lg:size-full w-full h-96'/>
-                </div>
-            </div>
+          <div className="bg-[#674F04] lg:size-full w-full h-96" />
         </div>
-
-    )
+      </div>
+    </div>
+  );
 }
--- my-app/app/(user)/map/page.tsx
+++ my-app/app/(user)/map/page.tsx
@@ -1,72 +1,80 @@
+"use client";
 
-'use client';
-
-import React from 'react'
-import { useState } from 'react'
-import {Autocomplete, AutocompleteProps, Avatar, Combobox, Group, Input, InputBase, useCombobox, Text} from '@mantine/core';
-import '@mantine/core/styles.css';
+import React from "react";
+import { useState } from "react";
+import {
+  Autocomplete,
+  AutocompleteProps,
+  Avatar,
+  Combobox,
+  Group,
+  Input,
+  InputBase,
+  useCombobox,
+  Text,
+} from "@mantine/core";
+import "@mantine/core/styles.css";
 
 const locationType = [
-  'Hotel',
-  'Point of interest',
-  'Spa',
-  'Cafe',
-  'Aquarium',
-  'Art gallery',
-  'Zoo',
-  'Museum',
-  'Church',
-  'Shopping Mall',
-  'Train Station',
-  'Restaurant'
-
+  "Hotel",
+  "Point of interest",
+  "Spa",
+  "Cafe",
+  "Aquarium",
+  "Art gallery",
+  "Zoo",
+  "Museum",
+  "Church",
+  "Shopping Mall",
+  "Train Station",
+  "Restaurant",
 ];
 
 export default function page() {
   type resDataType = {
-    statusCode: number,
-    data?:{
-      predictions: any[]
-    }
-  }
+    statusCode: number;
+    data?: {
+      predictions: any[];
+    };
+  };
 
   type resDataTypeGeo = {
-    statusCode: number,
-    data?:{
-      html_attributions: [],
-      result:{
+    statusCode: number;
+    data?: {
+      html_attributions: [];
+      result: {
         geometry: {
-          location:{
-            lat: any,
-            lng: any
-          }
-        }
-      }
-      }
-  }
+          location: {
+            lat: any;
+            lng: any;
+          };
+        };
+      };
+    };
+  };
 
   type resDataNearby = {
-    statusCode: number,
-    data?:{
-      html_attributions: [],
-      results: any
-    }
-  }
+    statusCode: number;
+    data?: {
+      html_attributions: [];
+      results: any;
+    };
+  };
 
-  const [text,setText] = useState('')
-  const [datas,setDatas] = useState<any[]>()
-  const [datas1,setDatas1] = useState<any[]>()
-  const [location,setLocation] = useState<any[]>(['central world'])
-  const [geo,setGeo] = useState<any[]>()
-  const [geoString,setGeoString] = useState('');
-  const [nearby,setNearby] = useState<any[]>()
-  const [type,setType] = useState('cafe')
+  const [text, setText] = useState("");
+  const [datas, setDatas] = useState<any[]>();
+  const [datas1, setDatas1] = useState<any[]>();
+  const [location, setLocation] = useState<any[]>(["central world"]);
+  const [geo, setGeo] = useState<any[]>();
+  const [geoString, setGeoString] = useState("");
+  const [nearby, setNearby] = useState<any[]>();
+  const [type, setType] = useState("cafe");
 
   const combobox = useCombobox({
     onDropdownClose: () => combobox.resetSelectedOption(),
   });
 
-  const [value, setValue] = useState<string>('');
+  const [value, setValue] = useState<string>("");
 
   const options = locationType.map((item) => (
     <Combobox.Option value={item} key={item}>
@@ -74,124 +82,143 @@ export default function page() {
     </Combobox.Option>
   ));
 
-
-  const googleMapAPI = `https://www.google.com/maps/embed/v1/search?key=AIzaSyC6LYgvdJqZ0QLoJXA7XKLHuaqPPzLY1Ac&q=${encodeURIComponent(location)}+${type}`
-  const googleNeabyLocationAPI = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=cruise&location=-33.8670522%2C151.1957362&radius=1500&type=restaurant&key=AIzaSyC6LYgvdJqZ0QLoJXA7XKLHuaqPPzLY1Ac'
+  const googleMapAPI = `https://www.google.com/maps/embed/v1/search?key=AIzaSyC6LYgvdJqZ0QLoJXA7XKLHuaqPPzLY1Ac&q=${encodeURIComponent(
+    location
+  )}+${type}`;
+  const googleNeabyLocationAPI =
+    "https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=cruise&location=-33.8670522%2C151.1957362&radius=1500&type=restaurant&key=AIzaSyC6LYgvdJqZ0QLoJXA7XKLHuaqPPzLY1Ac";
 
   async function handleChange(e: any) {
     setText(e);
-    const resAutocomplete = await fetch(`/api/autocomplete/detail?query=${encodeURIComponent((e) as string)}`)
-    if (resAutocomplete.ok){
-      const resData:resDataType = await resAutocomplete.json()
-      setDatas(resData.data?.predictions)
-      setDatas1(resData.data?.predictions)
+    const resAutocomplete = await fetch(
+      `/api/autocomplete/detail?query=${encodeURIComponent(e as string)}`
+    );
+    if (resAutocomplete.ok) {
+      const resData: resDataType = await resAutocomplete.json();
+      setDatas(resData.data?.predictions);
+      setDatas1(resData.data?.predictions);
       const geoString = geo?.join("%2C");
-      if (geoString !== undefined){
-        setGeoString(geoString)
+      if (geoString !== undefined) {
+        setGeoString(geoString);
       }
-
     }
-    
   }
-  const newArray = datas?.map(subArray => subArray.description);
-  const nearby_name = nearby?.map(subArray => subArray.business_status);
-  const nearby_status = nearby?.map(subArray => subArray.name  );
-  const newArray1 = datas1?.map(subArray => subArray.place_id)?.[0];
+  const newArray = datas?.map((subArray) => subArray.description);
+  const nearby_name = nearby?.map((subArray) => subArray.business_status);
+  const nearby_status = nearby?.map((subArray) => subArray.name);
+  const newArray1 = datas1?.map((subArray) => subArray.place_id)?.[0];
 
-  function locationSet(e:any){
-    setLocation(e)
-    }
-
-  async function locationAdd (e:any) {
-    setLocation(e)
-    const resGeo = await fetch(`/api/explore/location-geo?place_id=${encodeURIComponent((newArray1) as string)}`)
-    if (resGeo.ok){
-      const resData1:resDataTypeGeo = await resGeo.json()
-      setGeo([resData1.data?.result.geometry.location.lat,resData1.data?.result.geometry.location.lng])
-    }
-    
-  
-    const resNearby = await fetch(`/api/explore/nearbysearch?location=${encodeURIComponent((geoString) as string)}&type=${encodeURIComponent((type) as string)}`)
-    if (resNearby.ok){
-      const resData2:resDataNearby = await resNearby.json()
-      setNearby(resData2.data?.results)
-    }
-    console.log(e)
+  function locationSet(e: any) {
+    setLocation(e);
   }
-  console.log(datas)
-  console.log(newArray)
-  console.log(nearby)
-  console.log(location)
 
-  
+  async function locationAdd(e: any) {
+    setLocation(e);
+    const resGeo = await fetch(
+      `/api/explore/location-geo?place_id=${encodeURIComponent(
+        newArray1 as string
+      )}`
+    );
+    if (resGeo.ok) {
+      const resData1: resDataTypeGeo = await resGeo.json();
+      setGeo([
+        resData1.data?.result.geometry.location.lat,
+        resData1.data?.result.geometry.location.lng,
+      ]);
+    }
+
+    const resNearby = await fetch(
+      `/api/explore/nearbysearch?location=${encodeURIComponent(
+        geoString as string
+      )}&type=${encodeURIComponent(type as string)}`
+    );
+    if (resNearby.ok) {
+      const resData2: resDataNearby = await resNearby.json();
+      setNearby(resData2.data?.results);
+    }
+    console.log(e);
+  }
+  console.log(datas);
+  console.log(newArray);
+  console.log(nearby);
+  console.log(location);
+
   return (
-<div>
-<div className='grid grid-cols-1 lg:grid-cols-2'>
-  <div>
-    <div  className='bg-[#674F04] w-3/4 h-56 mt-32 mx-32'>
-      <div className='flex justify-center items-center mt-12'>
-        <div className='relative max-w-[640px] w-full px-4 mb-10 mt-12'>
-          <Autocomplete data={newArray} onChange={handleChange} onOptionSubmit={locationAdd}/>
-        </div>
-      </div>
-      <Combobox
-        store={combobox}
-        withinPortal={false}
-        onOptionSubmit={(val) => {
-          setValue(val);
-          setType(val.toLowerCase());
-          combobox.closeDropdown();
-        }}
-      >
-        <Combobox.Target>
-          <InputBase
-            component="button"
-            type="button"
-            pointer
-            rightSection={<Combobox.Chevron />}
-            onClick={() => combobox.toggleDropdown()}
-            rightSectionPointerEvents="none"
-          >
-            {value || <Input.Placeholder>Pick value</Input.Placeholder>}
-          </InputBase>
-        </Combobox.Target>
-
-        <Combobox.Dropdown>
-          <Combobox.Options>{options}</Combobox.Options>
-        </Combobox.Dropdown>
-      </Combobox>
-    </div>
     <div>
-      <div  className='bg-[#9D864F] w-3/4 h-96 my-8 mx-32'>
-      {nearby && (
-                <div className='card bg-[#F5F0E8] py-10'>
-                    <div className='grid grid-cols-1 '>
-                    {nearby.map((datas:any, index:any) => (
-                            <div key={index} className="card-top rounded-2xl shadow-md text-[#674F04] bg-[#F5F5F5] pb-5"> 
-                                  <div>{datas.business_status}</div>
-                                  <div onClick={(val) => {setLocation(datas.name);}} >{datas.name}</div>
+      <div className="grid grid-cols-1 lg:grid-cols-2">
+        <div>
+          <div className="bg-[#674F04] w-3/4 h-56 mt-32 mx-32">
+            <div className="flex justify-center items-center mt-12">
+              <div className="relative max-w-[640px] w-full px-4 mb-10 mt-12">
+                <Autocomplete
+                  data={newArray}
+                  onChange={handleChange}
+                  onOptionSubmit={locationAdd}
+                />
+              </div>
+            </div>
+            <Combobox
+              store={combobox}
+              withinPortal={false}
+              onOptionSubmit={(val) => {
+                setValue(val);
+                setType(val.toLowerCase());
+                combobox.closeDropdown();
+              }}
+            >
+              <Combobox.Target>
+                <InputBase
+                  component="button"
+                  type="button"
+                  pointer
+                  rightSection={<Combobox.Chevron />}
+                  onClick={() => combobox.toggleDropdown()}
+                  rightSectionPointerEvents="none"
+                >
+                  {value || <Input.Placeholder>Pick value</Input.Placeholder>}
+                </InputBase>
+              </Combobox.Target>
+
+              <Combobox.Dropdown>
+                <Combobox.Options>{options}</Combobox.Options>
+              </Combobox.Dropdown>
+            </Combobox>
+          </div>
+          <div>
+            <div className="bg-[#9D864F] w-3/4 h-96 my-8 mx-32">
+              {nearby && (
+                <div className="card bg-[#F5F0E8] py-10">
+                  <div className="grid grid-cols-1 ">
+                    {nearby.map((datas: any, index: any) => (
+                      <div
+                        key={index}
+                        className="card-top rounded-2xl shadow-md text-[#674F04] bg-[#F5F5F5] pb-5"
+                      >
+                        <div>{datas.business_status}</div>
+                        <div
+                          onClick={(val) => {
+                            setLocation(datas.name);
+                          }}
+                        >
+                          {datas.name}
                         </div>
-                      ))}
-                    </div>
+                      </div>
+                    ))}
+                  </div>
                 </div>
-                )}
-      </div>
-    </div>
-  </div>
-        <div className='container my-32 mx-auto text-black'>
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="container my-32 mx-auto text-black">
           <iframe
             width="600"
             height="600"
             frameBorder="0"
             src={googleMapAPI}
-            >
-          </iframe>
+          ></iframe>
         </div>
       </div>
     </div>
-
-  )
+  );
 }
-
-
-
