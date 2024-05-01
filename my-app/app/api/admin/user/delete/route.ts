0e50993c954a2d1910234a92cdffe5164c71d847
dstorvia <teerapong.pong@kmutt.ac.th>
2024-05-01 21:32:55 +0700

    Admin dashboard for user

---
+++ my-app/app/api/admin/user/delete/route.ts
@@ -0,0 +1,44 @@
+import prisma from "@/app/lib/prisma"
+import { getServerSession } from "next-auth/next"
+import authOptions from "@/app/lib/AuthProvider"
+
+type RequestData = {
+    id:string,
+    username:string,
+    email:string
+}
+
+export async function DELETE(request: Request) {
+    try {
+        const res: RequestData = await request.json();
+        const deleteUser = await prisma.user.delete({
+            where: {
+                id: res.id
+            }
+        });
+
+        if (deleteUser) {
+            return new Response(JSON.stringify({
+                statusCode: 200,
+                message: "ลบข้อมูลเรียบร้อยแล้ว"
+            }), {
+                status: 200
+            });
+        }
+
+        return new Response(JSON.stringify({
+            statusCode: 400,
+            message: "เกิดข้อผิดพลาดในการลบข้อมูล"
+        }), {
+            status: 400
+        });
+        
+    } catch (error:any) {
+        return new Response(JSON.stringify({
+            statusCode: 400,
+            message: error.toString()
+        }), {
+            status: 400
+        });
+    }
+}
---
+++ my-app/app/api/admin/user/route.ts
@@ -0,0 +1,32 @@
+import prisma from "@/app/lib/prisma"
+import { getServerSession } from "next-auth/next"
+import authOptions from "@/app/lib/AuthProvider"
+
+export async function GET(request: Request) {
+    try {
+        const userData = await prisma.user.findMany({
+            select:{
+                id:true,
+                username:true,
+                email:true,
+                createdAt:true
+            }
+    })
+
+        return new Response(JSON.stringify({
+            statusCode:200,
+            data:userData
+        }) , {
+            status:200
+        })
+
+    } catch (error) {
+        console.log(error)
+        return new Response( JSON.stringify({
+            statusCode: 500,
+            message: 'เกิดข้อผิดพลาด โปรดลองใหม่อีกครั้ง'
+        }) , {
+            status: 500
+        })
+    }
+}
